ea5f733dc0453abfa8bad3c06cc0a3c78fe29f69
Gabriel Pigatto <gabrielpigatto@live.com>
2022-01-11 09:26:58 -0300

    Initial Commit

---
+++ card_wars/card_warsApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_warsApp.swift
+//  card_wars
+//
+//  Created by Gabriel Pigatto on 11/01/22.
+//
+
+import SwiftUI
+
+@main
+struct card_warsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
